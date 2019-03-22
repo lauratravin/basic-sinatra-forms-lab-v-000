1a4e45023d5b3ba12a2d5355b2e90cd460987a5f
Laura Messa Travin <laura.travin@gmail.com>
2019-03-22 20:58:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,7 +10,11 @@ class App < Sinatra::Base
       # binding.pry
         @name = params[:name]
         @coach =  params[:coach]
-
+        @pg =  params[:pg]
+        @sg =  params[:sg]
+        @sf =  params[:sf]
+        @pf =  params[:pf]
+        @c =  params[:c]
       erb :team
     end
 
